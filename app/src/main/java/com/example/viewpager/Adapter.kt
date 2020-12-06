ceca1f74b1b7c4a4cff481ed08dccbb7f09676bd
edwina1114 <ta517152139@gmail.com>
2020-12-06 18:05:05 +0800

    #View Pager + Tab layout (HOME PAGE BAR)

--- app/src/main/java/com/example/viewpager/Adapter.kt
+++ app/src/main/java/com/example/viewpager/Adapter.kt
@@ -14,7 +14,8 @@ class Adapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentM
             4 -> return vegetable_f()
             5 -> return fruit_f()
             6 -> return dessert_f()
-            else -> return breakfast_f()
+            7 -> return breakfast_f()
+            else -> return qaz_f()
         }
     }
 
@@ -31,7 +32,8 @@ class Adapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentM
             4 -> return "Vegetable"
             5 -> return "Fruit"
             6 -> return "Dessert"
-            else -> return "Breakfast"
+            7 -> return "Breakfast"
+            else -> return "qaz"
         }
     }
 }
